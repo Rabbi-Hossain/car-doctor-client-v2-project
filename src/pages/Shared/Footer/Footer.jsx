f80f9cecaac93b339b2ae4ffa925aac0fc35975c
Rabbi hossain <rh7391679@gmail.com>
2024-07-03 20:26:07 -0700

    add a new text

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,6 +3,7 @@ const Footer = () => {
     return (
         <footer className="footer p-10 bg-base-200 text-base-content">
             <div>
+                <h2 className='text-lg font-semibold'>demo session</h2>
                 <img src={logo} alt="" />
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
             </div>
